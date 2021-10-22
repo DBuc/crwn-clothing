03173458ba074772c9669bfb43cf8abe075b0794
David Buchanan <dbuchana@verizon.net>
2021-10-22 13:50:30 -0400

    adding header comonent and links to home ...

--- src/App.js
+++ src/App.js
@@ -2,25 +2,15 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
-/*const HomePage = (props) => {
-  console.log("HomePage props=", props)
-  return (<div><h1>HOME PAGE</h1></div>)
-};
+import './App.css';
 
-const TopicsList = (props) => {
-  console.log("TopicsList props=", props)
-  return (<div><h1>HATS PAGE</h1></div>);
-};
-const TopicDetail = (props) => {
-  console.log("TopicDetail props=", props)
-  return (<div><h1>TOPIC DETAIL PAGE</h1></div>)
-};
-*/
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
