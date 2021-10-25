f502f24b1c1c0c1fe4a4907102760bec6ac6c440
David Buchanan <dbuchana@verizon.net>
2021-10-25 15:27:09 -0400

    updated shop data with new data, updated cart utils to group items in cart reducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,14 @@
+import { resultingClientExists } from "workbox-core/_private";
+
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
+    if ( existingCartItem ) {
+        return cartItems.map( cartItem => 
+            cartItem.id === cartItemToAdd.id 
+            ? { ...cartItem, quantity: cartItem.quantity + 1 } 
+            : cartItem
+            )
+    }
+
+    return [...cartItems, { ...cartItemToAdd, quantity: 1}]
+}
